a659747a073c2ec5e13ab8b1ba784aa36ea2ed94
Wojciech Kaczmarzyński <wojciech.kaczmarzynski@gmail.com>
2020-11-08 12:38:44 +0100

    Task 5.3 5.4 fixed - add functions.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 let randomNumber = Math.floor(Math.random() * 3 + 1);
-​
+
 console.log('Wylosowana liczba to: ' + randomNumber);
-​
+
 let computerMove = 'nieznany ruch';
-​
+
 if(randomNumber == 1){
   computerMove = 'kamień';
 } else if (randomNumber == 2){
@@ -11,7 +11,7 @@ if(randomNumber == 1){
 } else {
     computerMove = 'nożyce';
 }
-​
+
 printMessage('Ruch komputera to: ' + computerMove);
 
 
